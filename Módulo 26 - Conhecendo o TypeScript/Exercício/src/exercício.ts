1f0c9236b78f7789d5a51902a5ab6cf6aac72f4e
Rafael Silverio <rafael.silverio.s@outlook.com>
2023-08-14 14:34:35 -0300

    exercício concluído

---
+++ Módulo 26 - Conhecendo o TypeScript/Exercício/src/exercício.ts
@@ -0,0 +1,11 @@
+function multiplicacao(n1: number, n2: number) {
+    return n1 * n2
+}
+console.log(multiplicacao(10, 20));
+
+
+function olaNome(nome:string) {
+
+    return `Olá, ${nome}!`;
+}
+console.log(olaNome('Rafael'));
