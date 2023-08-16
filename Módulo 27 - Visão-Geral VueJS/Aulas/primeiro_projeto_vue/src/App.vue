2cec7f2c631eb22802c569595aabb4de26e8aae0
Rafael Silverio <rafael.silverio.s@outlook.com>
2023-08-16 12:05:24 -0300

    até aula 4

--- Módulo 27 - Visão-Geral VueJS/Aulas/primeiro_projeto_vue/src/App.vue
+++ Módulo 27 - Visão-Geral VueJS/Aulas/primeiro_projeto_vue/src/App.vue
@@ -1,47 +1,76 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+const nome = 'Rafael';
+const meuObj = {
+  nome: "Rafael",
+  filmeFavorito: "Rocky"
+}
+
+function dizOla(nome) {
+  return `${nome} diz oi`;
+}
+
+const enderecoImagem = "https://i.pinimg.com/550x/d1/a0/68/d1a06896b699ab13d8fda6b26cdb96e5.jpg";
+const enderecoImagemSuperman = "https://cdn.pixabay.com/photo/2020/07/11/04/41/superman-5392758_1280.png";
+
+const botaoDesabilitado = true;
+
+const gostaDoBatman = true;
+const gostaDoSuperman = true;
+
+const estaAutorizado = true;
+
+// let contador = 0;
+const estado = reactive({
+  contador: 0,
+  email: '',
+});
+
+function incrementar() {
+  estado.contador++;
+}
+
+function decrementar() {
+  estado.contador--;
+}
+
+function alteraEmail(evento) {
+  estado.email = evento.target.value
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <button :disabled="botaoDesabilitado">Enviar Mensagem</button>
+  <h1>{{nome}}</h1>
+  <h2>{{ meuObj.filmeFavorito }}</h2>
+  <h3>{{ dizOla("Batman") }}</h3>
+  <img v-if="gostaDoBatman" :src="enderecoImagem" alt="Imagem do Batman">
+  <img v-else-if="gostaDoSuperman" :src="enderecoImagemSuperman" alt="">
+  <h2 v-else>Não curte heróis da DC</h2>
+  <img v-show="gostaDoSuperman" :src="enderecoImagemSuperman" alt="">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <h3 v-if="estaAutorizado">Bem-Vindo</h3>
+  <h3 v-else="">Não foi desta vez</h3>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <br>
+  <hr>
+
+  {{ estado.contador }}
+
+  <button @click="incrementar" type="button">+</button>
+  <button @click="decrementar" type="button">-</button>
+
+  <br>
+  <hr>
+
+  {{ estado.email }}
+  <!-- <input type="email" @keyup="evento => estado.email = evento.target.value"> -->
+  <input type="email" @keyup="alteraEmail">
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+img{
+  height: 500px;
 }
 </style>
