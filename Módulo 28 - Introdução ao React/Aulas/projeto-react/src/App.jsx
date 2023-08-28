e9de4ce339b4ce6a737e2bc2fe6a27e0985dcfd6
Rafael Silverio <rafael.silverio.s@outlook.com>
2023-08-28 16:03:31 -0300

    Aulas do modulo finalizadas

--- Módulo 28 - Introdução ao React/Aulas/projeto-react/src/App.jsx
+++ Módulo 28 - Introdução ao React/Aulas/projeto-react/src/App.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Perfil from "./components/Perfil";
+import Formulario from "./components/Formulario";
+import ReposList from "./components/RepoList";
 
 function App() {
+  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState (true);
+  const [nomeUsuario, setNomeUsuario] = useState('');
   // const nome = "Rafael"
 
   // function retornaNome() {
@@ -15,7 +20,19 @@ function App() {
 
   return (
     <>
-      <Perfil />
+      <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
+
+      {nomeUsuario.length > 4 && (
+        <>
+          <Perfil nomeUsuario={nomeUsuario} />
+          <ReposList nomeUsuario={nomeUsuario} />
+        </>
+      )}
+
+      {/* {formularioEstaVisivel && (
+      <Formulario />
+      )}
+      <button onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">Toggle Form</button> */}
       {/* <h1>Olá, {retornaNome()} e {pessoa.nome}</h1> */}
       {/* <h2>Subtítulo</h2> */}
       {/* {estaDeDia === true ? 'Bom dia' : 'Boa tarde'} */}
--- Módulo 28 - Introdução ao React/Aulas/projeto-react/src/main.jsx
+++ Módulo 28 - Introdução ao React/Aulas/projeto-react/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import './global.css'
+
 // import {PI as numeroPi, GTM_BRASIL} from './teste'
 // import nomes from './nomes.js'
 
